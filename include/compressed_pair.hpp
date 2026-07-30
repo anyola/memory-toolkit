0a566cb7252c258b71d52a7c4c8bf004f93e886f
Alyona-slukinova <slukinova.alyona@gmail.com>
2026-07-30 08:33:12 +0500

    feat: add CompressedPair

---
+++ include/compressed_pair.hpp
@@ -0,0 +1,47 @@
+#ifndef COMPRESSED_PAIR_HPP
+#define COMPRESSED_PAIR_HPP
+
+#include <type_traits>
+#include <utility>
+
+namespace mtk {
+    template<typename T1, typename T2, bool = std::is_empty_v<T2> && !std::is_final_v<T2>>
+    class CompressedPair;
+
+    template<typename T1, typename T2>
+    class CompressedPair<T1, T2, true> : private T2 {
+    private:
+        T1 first;
+    public:
+        CompressedPair() = default;
+        CompressedPair(T1 f, T2 s) : T2(std::move(s)), first(std::move(f)) {}
+
+        T1& get_first() noexcept {
+            return first;
+        }
+        T2& get_second() noexcept {
+            return *this;
+        }
+    };
+    
+    template<typename T1, typename T2>
+    class CompressedPair<T1, T2, false> {
+    private:
+        T1 first;
+        T2 second;
+    public:
+        CompressedPair() = default;
+        CompressedPair(T1 f, T2 s) : first(std::move(f)), second(std::move(s)) {}
+
+        T1& get_first() noexcept {
+            return first;
+        }
+        T2& get_second() noexcept {
+            return second;
+        }
+
+    };
+
+}
+
+#endif
